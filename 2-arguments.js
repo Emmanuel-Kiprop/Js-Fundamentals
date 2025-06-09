e63e6e50dbcff85a1ecce5609530aab070660f50
emmanuel <emmanuelkiprop2024@gmail.com>
2025-06-09 12:10:38 +0300

    Task 2: Script responds to number of arguments

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2); // This collects the arguments you type after the filename
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
